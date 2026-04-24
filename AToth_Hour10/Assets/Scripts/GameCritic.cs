aa766b18ebcd90a7271648095a87260a06e8b59d
At0thclass <atoth001@student.sdccd.edu>
2026-04-24 12:27:52 -0700

    Hour 10 Assignment

---
+++ AToth_Hour10/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,10 @@
+//Q1 /*Right now, my game isn't really that difficult. It does feel hard to affect the balls with the bumper*/
+//Q2 /* I could maybe expand the bumper a bit, or move the balls around to different locations to*/
+//Q3 /* improvements to some of the visuals/textures? Other than that I'm not sure*/
+//Q4 /*It's kinda fun to watch the balls bounce around, but trying to get them into the goals with the bumper is annoying*/
+
+//Changes Made:
+    //I increased the size of the bumper from (3.5,3,1), to (6,3,1) I also changed the X rotation value from 43 to 0
+    //I made 2 variations of the ChaosBall. One is bigger and slower moving, while the other is smaller and faster
+    //I created a goal for the ChaosBalls
+    //I added another modified cube to the bumper to create more sides for the balls to bounce off (makes it a little easier)

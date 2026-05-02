3df2afe19b0f42872da91926d9baf7601feb2a7d
At0thclass <atoth001@student.sdccd.edu>
2026-05-01 23:04:36 -0700

    10 lamps

--- AToth_Hour11/Assets/Scripts/PrefabGenerator.cs
+++ AToth_Hour11/Assets/Scripts/PrefabGenerator.cs
@@ -6,19 +6,47 @@ public class PrefabGenerator : MonoBehaviour
 {
     public GameObject prefab;
 
+    [Header("Line Spawn Settings")]
+    public int numberToSpawn = 10;
+    public float spacing = 2f;           
+    public Vector3 spawnDirection = Vector3.right;  
+
     void Update()
     {
-        // Whenever we hit the B key we will generate a prefab at the
-        // position of the original prefab
-        // Whenever we hit the space key, we will generate a prefab at the
-        // position of the spawn object that this script is attached to
+   
         if (Input.GetKeyDown(KeyCode.B))
         {
             Instantiate(prefab);
         }
+
         if (Input.GetKeyDown(KeyCode.Space))
         {
             Instantiate(prefab, transform.position, transform.rotation);
         }
+
+ 
+        if (Input.GetKeyDown(KeyCode.X))
+        {
+            SpawnInLine();
+        }
+    }
+
+    void SpawnInLine()
+    {
+        if (prefab == null)
+        {
+            Debug.LogError("Prefab is not assigned!");
+            return;
+        }
+
+        Vector3 startPosition = transform.position;
+
+        for (int i = 0; i < 10; i++)
+        {
+            Vector3 spawnPos = startPosition + (spawnDirection * spacing * i);
+            Instantiate(prefab, spawnPos, transform.rotation);
+        }
+
+        Debug.Log($"Spawned {10} prefabs in a line!");
     }
 }

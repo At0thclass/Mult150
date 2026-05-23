62938bbe01723c218032892d7f1aa0ae3c4f54fd
At0thclass <atoth001@student.sdccd.edu>
2026-05-22 23:16:32 -0700

    Hour 20

---
+++ AToth_Hour20/Assets/Scripts/GameCritic.cs
@@ -0,0 +1,23 @@
+/*I think the game is relateively easy at the moment. The amount of power-ups makes it so that you can play 
+ * for a while before you get the "game over" message. */
+
+/* I'd either reduce the number of power-ups, or increase the number of obstacles.
+ * I could also just reduce the power of the power-ups or increase the penalty of running into obstacles */
+
+/* Maybe the game would benefit from having some unique music while you play? 
+ * Additional difficulty levels? */
+
+/* The Game can be fun when there's a power-up in front of a obstacle, and you have to try to grab
+ * the power up and then avoid the obstacle */
+
+/* I'm not sure that there's any part of the game that is tedious, but I think the movement is a bit
+ * slippery */
+
+/*Power-Up changed to a cylinder with the following scale:
+ * (0.75,0.18,0.01)
+ * Particles changed to Billboard render mode, Hemisphere shape has been kept but I changed the arc to 90
+ * and the mode to Ping-Pong*/
+
+/*Obstacle was changed to a sphere with the following scale: (2,0.2,2)
+ * added a cylinder and a capsule to make a sort of lava/chemical spill 
+ * texture is lava/chemical */

147efbc501fee5f52d3e7f08710477a9cfde981f
At0thclass <atoth001@student.sdccd.edu>
2026-05-30 00:44:35 -0700

    final

---
+++ AToth_Final/Assets/Scripts/Modifications.cs
@@ -0,0 +1,48 @@
+﻿public class Modifications
+{/*
+
+    /*  ================================================================
+        GAUNTLET RUNNER
+        ================================================================
+        1 - directional light (colour, intensity, etc)
+        modifications: changed to light green, intensity of 0.5
+
+        2 - fog
+        modifications: fog given a density of 0.025 and an exponential squared mode
+
+        3 - create & apply skybox and customise it (atmosphere thickness, sky tint, exposure, etc)
+        modifications: N/A
+        
+        OBSTACLE PREFAB
+        4 - add or modify light component (colour, range, intensity, etc)
+        modifications: removed stretched capsule (weird hitbox), changed light mode to baked & 
+        intensity to 5
+        
+        POWERUP PREFAB
+        5 - replace with a new complex object comprised of 3 or more shapes (different from previously made object)
+        modifications: power up is now a mushroom, with a capsule "stem" and sphere "top"
+
+        6 - use gameobject hierarchy to keep the powerup prefab organised
+        [no record necessary]
+
+        7 - apply at least 2 new textures
+        (add source url or package name)
+        texture source 1: https://minecraft.novaskin.me/post/4830503135/rk-kawaii-texture-mushroom-stem
+        texture source 2: https://www.publicdomainpictures.net/en/view-image.php?image=306677&picture=polka-dots-red-white
+
+        8 - add particle system component and customise at least 3 parameters
+        parameter 1: stretched billboard render mode
+        parameter 2: hemisphere shape
+        parameter 3: start speed = 3
+
+        WALL
+        9 - convert a wall to prefab, create a variant, and use the variant to replace the other wall in the scene
+        [no record necessary]
+
+        10 - modify wall prefab material tint so that the 2 walls are different in appearance or colour
+        [no record necessary]
+     
+     */
+
+
+}
